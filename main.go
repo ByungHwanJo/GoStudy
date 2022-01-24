edce992839b19caef9fac6c74956a2ed3aa55744
191823 <bhjo@shinsegae.com>
2022-01-24 17:57:37 +0900

    DB연결 연섭.

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
 
 func main() {
 	fmt.Println("Hellow World")
+
+	condb, errdb := sql.Open("mssql", "server=10.253.12.19;user id=sa;password=tlstprP1@#;")
+	if errdb != nil {
+		fmt.Println("  Error open db:", errdb.Error())
+	}
+
+	defer condb.Close()
 }
